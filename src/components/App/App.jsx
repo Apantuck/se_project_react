4b62969c341a7775203b27b9ed6181e44b181b9f
Alexa Pantuck <apantuck@gmail.com>
2025-09-04 17:04:21 -0700

    Styled ModalWithForm component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Footer from "../Footer/Footer.jsx";
 import Header from "../Header/Header.jsx";
 import Main from "../Main/Main.jsx";
+import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 import { defaultClothingItems } from "../../utils/clothingItems.js";
 
 function App() {
   const [clothingItems, setClothingItems] = useState(defaultClothingItems);
   const [weatherData, setWeatherData] = useState({ type: "warm" });
+  const [activeModal, setActiveModal] = useState("");
 
   return (
     <div className="app">
@@ -16,6 +18,7 @@ function App() {
         <Main clothingItems={clothingItems} weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm />
     </div>
   );
 }
---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,57 @@
+import "./ModalWithForm.css";
+import closeIcon from "../../assets/icon-close-default.svg";
+
+function ModalWithForm() {
+  return (
+    <div className="modal">
+      <div className="modal__container">
+        <h2 className="modal__title">New garment</h2>
+        <button
+          type="button"
+          className="modal__btn-close"
+          style={{ backgroundImage: `url(${closeIcon})` }}
+        ></button>
+        <form className="modal__form">
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              type="text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="imageURL" className="modal__label">
+            Image{" "}
+            <input
+              type="url"
+              className="modal__input"
+              id="imageURL"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-btns">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label htmlFor="hot" className="modal__label modal__label_radio">
+              <input type="radio" className="modal__input-radio" id="hot" />
+              Hot
+            </label>
+            <label htmlFor="warm" className="modal__label modal__label_radio">
+              <input type="radio" className="modal__input-radio" id="warm" />
+              Warm
+            </label>
+            <label htmlFor="cold" className="modal__label modal__label_radio">
+              <input type="radio" className="modal__input-radio" id="cold" />
+              Cold
+            </label>
+          </fieldset>
+          <button type="submit" className="modal__btn-submit">
+            Add garment
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
